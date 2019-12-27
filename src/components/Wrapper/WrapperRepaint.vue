d8ffc4a104badbe2da9b6953a7b07ebce206f311
lucas <lucas.paetow@gmail.com>
2019-12-27 14:25:43 +0100

    add raf Animation wrapper

--- src/components/Wrapper/WrapperRepaint.vue
+++ src/components/Wrapper/WrapperRepaint.vue
@@ -1,127 +1,169 @@
 <template>
-  <!--     @before-enter="beforeEnter"
-        @enter="enter"
-        @after-enter="afterEnter"-->
-  <transition
-    @before-leave="beforeLeave"
-    @leave="leave"
-    @after-leave="afterLeave"
-    v-resize="resizeEvent"
-  >
-    <slot />
-  </transition>
+	<transition
+		@before-leave="beforeTransition"
+		@leave="whileTransition"
+		@after-leave="afterTransition"
+		@before-enter="beforeTransition"
+		@enter="whileTransition"
+		@after-enter="afterTransition"
+	>
+		<slot />
+	</transition>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { resize } from "@/utility/customDirectives/resize.js";
+import * as easing from "@/utility/easings/easings.js";
 
 export default {
-  components: {},
-  props: {
-    animationProperties: {
-      type: Array,
-      default: () => {
-        return ["width", "height"];
-      }
-    }
-  },
-  name: "about",
-  directives: {
-    resize
-  },
-  data() {
-    return {
-      previousValue: {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        width: 0,
-        height: 0,
-        x: 0,
-        y: 0
-      },
-      initalValue: null
-    };
-  },
-  methods: {
-    beforeLeave(el) {
-      this.initalValue = this.calculateValues(el);
-    },
-    afterLeave(el) {
-      console.log("done", el);
-    },
-    async leave(el, done) {
-      let previousValues = this.calculateValues(el);
+	props: {
+		animationProperties: {
+			type: Array,
+			default: () => {
+				return ["width"];
+			}
+		},
+		easeOut: {
+			type: String,
+			default: "easeOutSine"
+		},
+		easeIn: {
+			type: String,
+			default: "easeInSine"
+		},
+		duration: {
+			type: Number,
+			default: 250
+		}
+	},
+	name: "rafWrapper",
+	data() {
+		return {
+			currentValue: null,
+			previousValue: null,
+			transitionState: null
+		};
+	},
+	methods: {
+		beforeTransition(el) {
+			this.transitionState = "beforeTransition";
+			this.previousValue = this.calculateValues(el);
+			this.changeChildElements(el, "hide");
+		},
+		async whileTransition(el, done) {
+			this.transitionState = "whileTransition";
+			this.currentValue = this.calculateValues(el);
 
-      await this.animate({
-        duration: 200,
-        timing(timeFraction) {
-          return timeFraction;
-        },
-        draw(progress) {
-          el.style.width =
-            previousValues.width - previousValues.width * progress + "px";
-        }
-      });
-      await done();
-    },
-    /*beforeEnter(el) {
-      console.log(el);
-    },
-    enter(el, done) {
-      console.log(el);
-      done();
-    },
-    afterEnter(el) {
-      console.log(el);
-  },*/
-    animate({ timing, draw, duration }) {
-      let start = performance.now();
-      return new Promise(resolve => {
-        requestAnimationFrame(function animate(time) {
-          // timeFraction goes from 0 to 1
-          let timeFraction = (time - start) / duration;
-          if (timeFraction > 1) timeFraction = 1;
-          // calculate the current animation state
-          let progress = timing(timeFraction);
-          draw(progress); // draw it
-          timeFraction < 1
-            ? requestAnimationFrame(animate)
-            : resolve("resolved animation");
-        });
-      });
-    },
-    calculateValues(element) {
-      console.log(Object.entries(element));
-      //if the component was not rendered before it only has the Vue Component Array entry not its own class entry
-      if (Object.entries(element).length === 1) return;
+			let currentValue = this.currentValue;
+			let previousValue = this.previousValue;
+			let animationProperties = this.animationProperties;
+			let duration = this.duration;
 
-      const {
-        top,
-        right,
-        bottom,
-        left,
-        width,
-        height,
-        x,
-        y
-      } = element.getBoundingClientRect();
-      return { top, right, bottom, left, width, height, x, y };
-    },
+			//get a direction for every property in terms of "open" and "close"
+			let animationDirection = await this.decideAnimationDirection(
+				currentValue,
+				previousValue,
+				animationProperties
+			);
 
-    resizeEvent() {
-      this.initalValue = this.calculateValues(this.$el);
-    }
-  },
-  computed: {
-    ...mapGetters({})
-  },
-  mounted() {
-    // get inital values and store them
-    this.initalValue = this.calculateValues(this.$el);
-  }
+			await this.animate({
+				duration,
+				timing: easing[this.easeOut],
+				draw(progress) {
+					//for every Property animate in the given direction
+					animationProperties.forEach(prop => {
+						if (animationDirection[prop] === "close") {
+							//decrements the element by relative progress steps
+							el.style[prop] =
+								currentValue[prop] -
+								currentValue[prop] * progress +
+								"px";
+						} else {
+							//increments the element to the current value by relative progress steps
+							el.style[prop] =
+								previousValue[prop] +
+								currentValue[prop] * progress +
+								"px";
+						}
+					});
+				}
+			});
+			await done();
+		},
+		afterTransition(el) {
+			this.transitionState = "afterTransition";
+			this.changeChildElements(el, "show");
+		},
+		animate({ timing, draw, duration }) {
+			let start = performance.now();
+			return new Promise(resolve => {
+				requestAnimationFrame(function animate(time) {
+					// timeFraction goes from 0 to 1
+					let timeFraction = (time - start) / duration;
+					if (timeFraction > 1) timeFraction = 1;
+					// calculate the current animation state
+					let progress = timing(timeFraction);
+					draw(progress); // draw it
+					timeFraction < 1
+						? requestAnimationFrame(animate)
+						: resolve("resolved animation");
+				});
+			});
+		},
+		calculateValues(element) {
+			//if the component was not rendered before it only has the Vue Component Array entry not its own class entry
+			if (Object.entries(element).length === 1) return;
+
+			const {
+				top,
+				right,
+				bottom,
+				left,
+				width,
+				height,
+				x,
+				y
+			} = element.getBoundingClientRect();
+			return { top, right, bottom, left, width, height, x, y };
+		},
+		decideAnimationDirection(current, previous, animationProperties) {
+			if (!current || !previous || !animationProperties)
+				throw "need all values";
+
+			let propertyDirections = {};
+
+			animationProperties.forEach(prop => {
+				propertyDirections[prop] =
+					previous[prop] < current[prop] ? "open" : "close";
+			});
+			return propertyDirections;
+		},
+		changeChildElements(element, status) {
+			if (element.children.length === 0) return;
+
+			let opacity = status === "hide" ? 0 : "";
+			let display = status === "hide" ? "none" : "";
+			let children = element.children;
+			//hides the children elements with a small visual duration
+			let changeOpacity = childArray =>
+				childArray.forEach(child => {
+					child.style.transitionDuration = `${this.duration / 10}ms`;
+					child.style.opacity = opacity;
+				});
+			//removes them from render tree so there is less to animate
+			let changeDisplay = childArray =>
+				childArray.forEach(child => {
+					child.style.display = display;
+				});
+
+			if (status === "hide") {
+				changeOpacity(children);
+				changeDisplay(children);
+			} else {
+				changeDisplay(children);
+				changeOpacity(children);
+			}
+		}
+	}
 };
 </script>
 
--- src/components/navigation/Sidebar.vue
+++ src/components/navigation/Sidebar.vue
@@ -1,34 +1,37 @@
 <template>
-  <aside class="sidebar"></aside>
+	<aside class="sidebar">
+		<h2>Hello2</h2>
+		<h3>Hello3</h3>
+	</aside>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 
 export default {
-  components: {},
-  props: {},
-  name: "sidebar",
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {
-    ...mapGetters({})
-  },
-  mounted() {}
+	components: {},
+	props: {},
+	name: "sidebar",
+	data() {
+		return {};
+	},
+	methods: {},
+	computed: {
+		...mapGetters({})
+	},
+	mounted() {}
 };
 </script>
 
 <style scoped lang="scss">
 .sidebar {
-  /* Positioning */
-  /* Box-model */
-  width: 10rem;
-  height: 100%;
-  /* Typography */
-  /* Visual */
-  background-color: lightblue;
-  /* Misc */
+	/* Positioning */
+	/* Box-model */
+	width: 10rem;
+	height: 100%;
+	/* Typography */
+	/* Visual */
+	background-color: lightblue;
+	/* Misc */
 }
 </style>
